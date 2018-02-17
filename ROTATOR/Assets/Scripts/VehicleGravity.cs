47430bbb1cb58b862626fc52a008a1a7b50d5f56
MaelstromALPHA <maelstrom.alpha@gmail.com>
2018-02-17 15:04:05 +0000

    Added reversable physics to the ship object
    This method of adding physics allows us to apply inverted physics on an object by object basis, instead of forcing the whole world to invert physics

---
+++ ROTATOR/Assets/Scripts/VehicleGravity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleGravity : MonoBehaviour {
+
+	// Public variables
+	public bool gravityReversed;
+
+	// Private variables
+	Rigidbody ship;
+
+	// Use this for initialization
+	void Start () {
+		// Get the rigidbody for the attached object
+		ship = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		if (gravityReversed) {
+			ship.AddForce (Vector3.up * Physics.gravity.magnitude);
+		} 
+		else {
+			ship.AddForce (-Vector3.up * Physics.gravity.magnitude);
+		}
+	}
+}
