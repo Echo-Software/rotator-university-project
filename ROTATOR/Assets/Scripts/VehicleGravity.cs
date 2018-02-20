0805d95f0165ca039b276836a3f3fe9c8cf6b94b
MaelstromALPHA <maelstrom.alpha@gmail.com>
2018-02-20 22:36:42 +0000

    Changed how vehicle gravity functions
    Previously vehicle gravity simulated real world gravity with a force being applied on the vertical axis. This force was positive or negative, depending on gravity being reversed or not.
    
    Now vehicle gravity works by shooting a raycast out onto a surface and checking for the "Track" tag. If this tag is found, the force applied will keep the vehicle hovering above the track. The script also checks the rotation of the normal face hit by the raycast and adjusts the rotation of the vehicle accordingly (this should allow us to work with loops and corkscrews) but this functionality is implemented incredibly simplistically and will need a lot of testing and refinement.

--- ROTATOR/Assets/Scripts/VehicleGravity.cs
+++ ROTATOR/Assets/Scripts/VehicleGravity.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class VehicleGravity : MonoBehaviour {
 
 	// Public variables
-	public bool gravityReversed;
 
 	// Private variables
 	Rigidbody ship;
+	RaycastHit hit;
+	Vector3 reflect;
 
 	// Use this for initialization
 	void Start () {
@@ -18,15 +19,36 @@ public class VehicleGravity : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.DrawLine (transform.position, hit.point, Color.red);
 	}
 
 	void FixedUpdate() {
+
+		/* Gravity (world emulation)
 		if (gravityReversed) {
 			ship.AddForce (Vector3.up * Physics.gravity.magnitude);
 		} 
 		else {
 			ship.AddForce (-Vector3.up * Physics.gravity.magnitude);
 		}
+		*/
+
+		// Gravity (based on raycast hit on track object)
+		if (Physics.Raycast (transform.position, -transform.up, out hit)) {
+			Debug.Log (hit.distance);
+			if (hit.transform.tag == "Track") {
+				if (hit.distance > 1.5) {
+					ship.AddForce (-transform.up * (Physics.gravity.magnitude / 10));
+				} 
+				else if (hit.distance < 1.5) {
+					ship.AddForce (transform.up * (Physics.gravity.magnitude / 10));
+				}
+				transform.rotation = Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation;
+			} 
+			else {
+				print ("Not a track object");
+			}
+		}
+
 	}
 }
