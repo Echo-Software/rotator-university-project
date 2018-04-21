b0e72d19ab39c4295468afcbfb1df1aeea64b891
Gab712 <Gab712@users.noreply.github.com>
2018-04-21 01:30:05 +0100

    Power-Ups
    Power-up script to make them rotate and bob up and down.
    Also scaled the power-ups by 0.5 to make them a little bigger

---
+++ ROTATOR/Assets/Scripts/FloatAndBob.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatAndBob : MonoBehaviour {
+
+    private int degreesPs = 20;
+    private float amplitude = 0.5f;
+    private int frequency = 1;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+	// Use this for initialization
+	void Start ()
+    {
+        posOffset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Spins on the Y-axis
+        transform.Rotate(new Vector3(0, Time.deltaTime * degreesPs, 0), Space.Self);
+
+        // Bob up and down
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
