e00328ecf3f6358f5bec906b0777c61632d6bd1f
Gab712 <Gab712@users.noreply.github.com>
2018-04-29 23:56:16 +0100

    Boostpad change
    The design of the boost pad has been changed

---
+++ ROTATOR/Assets/Scripts/BoostPad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostPad : MonoBehaviour {
+
+    private int speed = 2;
+    private Material padM;
+
+	// Use this for initialization
+	void Start ()
+    {
+        padM = GetComponent<Renderer>().material;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 offset = new Vector2(-Time.time * speed % 1, 0);
+        padM.SetTextureOffset("_MainTex", offset);	
+	}
+}
